5e79db24697d52d2fba35612f48916a475c0cb41
Daniel Okello <handsdani@gmail.com>
2023-06-08 21:55:16 +0300

    Added swagger support

--- ProjectSkeleton/Program.cs
+++ ProjectSkeleton/Program.cs
@@ -16,8 +16,24 @@ builder.Services.AddApplication()
                 .AddInfrustructure()
                 .AddAPresentation();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Title = builder.Environment.ApplicationName,
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
+        $"{builder.Environment.ApplicationName} v1"));
+}
 
 app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
