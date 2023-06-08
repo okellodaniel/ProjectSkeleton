bbaabf97ac44a2aecb90bf3f37a880f8a521a9cf
Daniel Okello <handsdani@gmail.com>
2023-06-08 22:19:42 +0300

    cc

--- ProjectSkeleton/Program.cs
+++ ProjectSkeleton/Program.cs
@@ -1,15 +1,10 @@
-using Application;
-using Infrastructure;
-using Microsoft.EntityFrameworkCore;
-using Presentation;
-using ProjectSkeleton.Data;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
-builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
-builder.Services.AddDbContext<TodoDb>(x => x.UseNpgsql(builder.Configuration.GetConnectionString("TodoDb")));
+//builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+//builder.Services.AddDbContext<TodoDb>(x => x.UseNpgsql(builder.Configuration.GetConnectionString("TodoDb")));
 
 
 //builder.Services.AddApplication()
