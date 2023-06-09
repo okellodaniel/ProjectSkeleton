c424766db49c09c9e961e935d157613384fed858
Daniel Okello <handsdani@gmail.com>
2023-06-09 20:19:58 +0300

    Added strongly typed Ids

--- Domain/Cutomer.cs
+++ Domain/Cutomer.cs
@@ -2,8 +2,11 @@
 {
     public class Customer
     {
-        public Guid Id { get; private set; }
+        public CustomerId Id { get; private set; }
         public string Name { get; private set; } = string.Empty;
         public string Email { get; private set; } = string.Empty;
     }
+
+    public record CustomerId(Guid Value);
+
 }
--- Domain/Order.cs
+++ Domain/Order.cs
@@ -4,14 +4,14 @@
     {
         public readonly HashSet<LineItem> _lineItems = new ();
         private Order(){}
-        public Guid Id { get; private set; }
-        public Guid CustomerId { get; private set; }
+        public OrderId Id { get; private set; }
+        public CustomerId CustomerId { get; private set; }
 
         public static Order Create(Customer customer)
         {
             var order = new Order()
             {
-                Id = Guid.NewGuid(),
+                Id = new OrderId(Guid.NewGuid()),
                 CustomerId = customer.Id,
             };
 
@@ -19,24 +19,8 @@
         }
         public void Add(Product product)
         {
-            var lineItem = new LineItem(Guid.NewGuid(), Id, product.Id, product.Price);
+            var lineItem = new LineItem(new LineItemId(Guid.NewGuid()),new OrderId(Guid.NewGuid()), product.Id, product.Price);
             _lineItems.Add(lineItem);
         }
-    }
-
-    public class LineItem
-    {
-        internal LineItem(Guid id,Guid orderId, Guid productId, Money price)
-        {
-            Id = id;
-            OrderId = orderId;
-            ProductId = productId;
-            Price = price;
-        }
-
-        public Guid Id { get; private set; }
-        public Guid OrderId { get; private set; }
-        public Guid ProductId { get; private set; }
-        public Money Price {get; private set; }
     }
 }
